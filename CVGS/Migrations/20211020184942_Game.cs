c990f4ed7acafc2216e2737fa436237a268c2441
Jake Koroman <jkoroman4252@conestogac.on.ca>
2021-10-20 17:22:39 -0400

    added views

---
+++ CVGS/Migrations/20211020184942_Game.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CVGS.Migrations
+{
+    public partial class Game : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Game",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Developer",
+                table: "Game",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Game",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Platform",
+                table: "Game",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Game");
+
+            migrationBuilder.DropColumn(
+                name: "Developer",
+                table: "Game");
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Game");
+
+            migrationBuilder.DropColumn(
+                name: "Platform",
+                table: "Game");
+        }
+    }
+}
